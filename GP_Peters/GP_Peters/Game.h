170248066329259f4e4dc364f96a591646b634a0
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2021-04-26 20:12:07 +0200

    Game Class added

---
+++ GP_Peters/GP_Peters/Game.h
@@ -0,0 +1,22 @@
+#include "SDL.h"
+#include <stdio.h>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int x, int y, int width, int height, bool fullscreen);
+	
+	void eventHandler();
+	void update();
+	void render();
+	void clean();
+	bool running() { return isRunning; }
+
+private:
+	bool isRunning;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	int cnt = 0;
+};
--- GP_Peters/GP_Peters/main.cpp
+++ GP_Peters/GP_Peters/main.cpp
@@ -1,13 +1,16 @@
-#include "SDL.h"
+#include "Game.h"
 
+Game* game = nullptr;
 int main(int argc, char *argv[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window *window = SDL_CreateWindow("Climb it", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	game = new Game();
+	//Initialisieren des Fensters
+	game->init("Climb it", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
 
-	SDL_SetRenderDrawColor(renderer, 100, 150, 100, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(3000);
+	while (game->running()) {
+		game->eventHandler();
+		game->update();
+		game->render();
+	}
+	game->clean();
 	return 0;
 }
