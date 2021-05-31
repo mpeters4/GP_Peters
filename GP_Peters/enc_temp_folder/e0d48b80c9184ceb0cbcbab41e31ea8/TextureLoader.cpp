8526c9ade4814c1d87c95dbc8d54b4ab18d52e05
mpeters4 <50985676+mpeters4@users.noreply.github.com>
2021-05-31 17:40:27 +0200

    remove temp Objects

--- GP_Peters/enc_temp_folder/e0d48b80c9184ceb0cbcbab41e31ea8/TextureLoader.cpp
+++
@@ -1,8 +0,0 @@
-#include "TextureLoader.h"
-
-SDL_Texture* TextureLoader::LoadTexture(const char* texture, SDL_Renderer* ren){
-	SDL_Surface* tempSur = IMG_Load(texture);
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSur);
-	SDL_FreeSurface(tempSur);
-	return tex;
-}
